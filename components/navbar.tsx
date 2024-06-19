ea49f7f04f8cc0e225ea48de68a4d7aafccc3893
Christopher Haris <christoharis2005@gmail.com>
2024-06-19 15:13:25 +0700

    fix : navbar fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -46,6 +46,37 @@ const menu = [
   },
 ];
 
+type ListItemProps = {
+  className?: string;
+  title: string;
+  children: React.ReactNode;
+};
+
+const ListItem = React.forwardRef<HTMLAnchorElement, ListItemProps>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            {...props}
+          >
+            <div className="text-base font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+              {children}
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
+ListItem.displayName = "ListItem";
+
 function Navbar() {
   return (
     <div className="fixed w-full top-0 z-50 backdrop-blur-sm">
@@ -157,8 +188,6 @@ function Navbar() {
                   <a
                     href="https://github.com/ChristopherHaris"
                     target="_blank"
-                    legacyBehavior
-                    passHref
                   >
                     <NavigationMenuLink
                       className={navigationMenuTriggerStyle()}
@@ -176,30 +205,5 @@ function Navbar() {
     </div>
   );
 }
-
-const ListItem = React.forwardRef(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-base font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  }
-);
-ListItem.displayName = "ListItem";
 
 export default Navbar;
